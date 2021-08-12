9e7b0492e52e2cf1d73ba93b39248e3b3eb5a6f8
spobrien1116 <87784429+spobrien1116@users.noreply.github.com>
2021-08-12 15:06:38 -0400

    Completed week 3 mastery classes on arrays

---
+++ Week3MasteryClassesArrays.java
@@ -0,0 +1,146 @@
+public class Week3MasteryClassesArrays {
+
+//      public static void main(String[] args) {
+
+//          // Given an array of integers, loop through the array print the original values and then add the integer value provided
+//          // by the user to each element stored in the array. Loop through the new values and print out each new value to the console.
+//          int x;
+// 		    int[] arr = { 5, 10, 44, -4, 10 };
+// 		    x = 10;
+// 		    addX(arr, x);
+//          }
+
+//          static void addX(int[] arr, int x) {
+// 		    // Add your code below this line 
+// 		    System.out.println("**Original**");
+// 		    for (int num : arr) {
+// 		        System.out.println(num);
+// 		    }
+// 		    System.out.println("**New Array**");
+// 		    for (int num : arr) {
+// 		        num += x;
+// 		        System.out.println(num);
+// 		    }
+// 	    }
+
+        // static int[] buildArray(int num) {
+        //     // Write your code below
+        //     int[] numArray = new int[num];
+        //     for (int i = 0; i < num; i++) {
+        //         numArray[i] = i;
+        //     }
+        //     return numArray;
+        // }
+        // // Do not change any of the code below this line
+        // public static void main(String[] args) {
+        //     int num;
+        //     num = 8;
+        //     System.out.println("Your array is: ");
+        //     System.out.println(Arrays.toString(buildArray(num)));
+        // }
+        // // Note that this Arrays.toString(buildArray(num))); line does not seem to be working in VSC, but worked in the mastery class
+
+        // static void reverseArray(int a, int b, int c, int d, int e) {
+        //     // Write your code below this line 
+        //     int[] firstArray = new int[5];
+        //     firstArray[0] = a;
+        //     firstArray[1] = b;
+        //     firstArray[2] = c;
+        //     firstArray[3] = d;
+        //     firstArray[4] = e;
+        //     for (int num : firstArray) {
+        //         System.out.println(num);
+        //     }
+        //     int[] secondArray = new int[5];
+        //     secondArray[0] = e;
+        //     secondArray[1] = d;
+        //     secondArray[2] = c;
+        //     secondArray[3] = b;
+        //     secondArray[4] = a;
+        //     for (int num : secondArray) {
+        //         System.out.println(num);
+        //     }
+        //     // Write your code above this line 
+        // }
+    
+        // public static void main(String[] args) {
+        //     int a,b,c,d,e;
+        //     a = 1;
+        //     b = 2;
+        //     c = 3;
+        //     d = 4;
+        //     e = 5;
+        //     reverseArray(a, b, c, d, e);
+        // }
+        // // This was my code submission, which works. Below is the given answer, which seems better.
+
+        // static void reverseArray(int a, int b, int c, int d, int e) {
+        //     int[] arr = {a, b, c, d, e};
+        //     int j = arr.length;
+            
+        //     int[] reverseArr = new int[j];
+        //     for ( int i = 0; i < arr.length; i++ ) {
+        //         reverseArr[j-1] = arr[i];
+        //         j--;
+        //     }
+            
+        //     for ( int x : arr ) {
+        //         System.out.println(x);
+        //     }
+            
+        //     for ( int y : reverseArr ) {
+        //         System.out.println(y);
+        //     }
+        // }
+    
+        // public static void main(String[] args) {
+        //     int a,b,c,d,e;
+        //     a = 1;
+        //     b = 2;
+        //     c = 3;
+        //     d = 4;
+        //     e = 5;
+        //     reverseArray(a, b, c, d, e);
+        // }
+
+        // static int createInt(int num1, int num2, int num3, int num4, int num5, int num6) {
+        //     // Write your code below this line 
+        //     int[] arrayInt = {num1, num2, num3, num4, num5, num6};
+        //     int last = arrayInt.length;
+        //     return arrayInt[last - 1];
+        // }
+    
+        // // Do not change the code below this line 
+        // public static void main(String[] args) {
+        //     int num1, num2, num3, num4, num5, num6; 
+        //     num1 = 9;
+        //     num2 = 8;
+        //     num3 = 7;
+        //     num4 = 6;
+        //     num5 = 5;
+        //     num6 = 4;
+            
+        //     int returnedInt = createInt(num1, num2, num3, num4, num5, num6);
+        //     System.out.println("The last number of the array is " + returnedInt);
+        // }
+        
+        // static void charArray(String userWord) {
+        //     // Add your code below this lien 
+        //     char[] wordToCharArray = new char[userWord.length()];
+        //     for (int i = 0; i < userWord.length(); i++) {
+        //         wordToCharArray[i] = userWord.charAt(i);
+        //         System.out.println(wordToCharArray[i]);
+        //     }
+        //     // Add your code above this line
+        // }
+    
+        // public static void main(String[] args) {
+        //     String word;
+        //     word = "Hello";
+        //     charArray(word);
+        // }
+
+        // // Very proud I got this on the first try :)
+
+        
+}
